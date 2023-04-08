b200a8dd1cc4eb4c4324c1412042d31317d703f6
Ololade Akinrinsola <ololadeakinrinsola@MACs-MacBook-Pro-3.local>
2023-04-08 16:28:40 +0100

    added links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import { Container, Heading, Text,Box,Flex, Link } from '@chakra-ui/react'
 import React from 'react';
 import {MdOutlineEmail} from 'react-icons/md';
-import {AiFillPhone, AiFillTwitterCircle, AiFillGithub} from 'react-icons/ai';
+import {AiFillPhone, AiFillGithub,AiFillLinkedin} from 'react-icons/ai';
 
 export const Contact = () => {
   return (
@@ -9,10 +9,10 @@ export const Contact = () => {
         <Heading align='center' size='3xl'>Contact me</Heading>
         <Text fontSize='20px' align='center' >You can reach out to me on <Link color='#ff0000'>email</Link> and let's collaborate together</Text>
         <Flex gap='30px'>
-            <Link><MdOutlineEmail size={'30'}/></Link>
-            <Link><AiFillPhone size={'30'} /></Link>
-            <Link><AiFillTwitterCircle size={'30'}/></Link>
-            <Link><AiFillGithub size={'30'} /></Link>
+            <Link target="_blank" href='mailto: ololadeakinrinsola@gmail.com'><MdOutlineEmail size={'30'} /></Link>
+            <Link target="_blank" href='https://www.linkedin.com/in/ololade-akinrinsola/'><AiFillPhone size={'30'} /></Link>
+            <Link target="_blank" href='https://www.linkedin.com/in/ololade-akinrinsola/'><AiFillLinkedin size={'30'} /></Link>
+            <Link target="_blank" href='https://github.com/lolaakinrinsola'><AiFillGithub size={'30'} /></Link>
         </Flex>
     </Box>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,19 +14,19 @@ const Project=styled.div`
 export const Projects = () => {
   const projectsDetails=[
 
-    {title:'Tech1M Juniors', githublink:'https://github.com/Temyturpe/Tech1m-group3-project', websiteLink:'https://tech1m-group3-project.vercel.app/', about:'Tech1M Juniors is a web application designed for children to learn various tech skills.', tech:'React, Firebase, JavaScript, TailWind'},
-    {title:'Little Lemon', githublink:'https://github.com/Lolaakinrinsola/little-lemon', websiteLink:'#', about:'Little lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment.',tech:'React, HTML, JavaScript, SCSS'},
-    {title:'Metaverse', githublink:'https://github.com/Lolaakinrinsola/metabnb', websiteLink:'https://metabnbzuri.vercel.app', image:'lola-portfolio/src/components/Images/metabnb.png', about:'Metaverse provide you access to luxury and affordable houses in the metaverse',tech:'React, SCSS, JavaScript, HTML'},
-    {title:'Fylo', githublink:'https://github.com/Lolaakinrinsola/fylo-landing-page', websiteLink:'https://lolaakinrinsola.github.io/fylo-landing-page/', about:'A landing page for a company', tech:'React, SCSS, JavaScript, HTML'},
-    {title:'Zuri FIRST', githublink:'#', websiteLink:'https://secondtaskhng.vercel.app', about:'A biodata page for zuri internship and validates form',tech:'React, SCSS, JavaScript, HTML'},
-    {title:'My Portfolio', githublink:'https://github.com/Lolaakinrinsola/lola-portfolio', websiteLink:'https://lola-portfolio.vercel.app/',tech:'React, Chakra-UI, JavaScript, HTML'}
+    {title:'Tech1M Juniors', githublink:'https://github.com/Temyturpe/Tech1m-group3-project', websiteLink:'https://tech1m-group3-project.vercel.app/', about:'Tech1M Juniors is a web application designed for children to learn various tech skills.', tech:'React, Firebase, JavaScript, TailWind',key:'1'} ,
+    {title:'Little Lemon', githublink:'https://github.com/Lolaakinrinsola/little-lemon', websiteLink:'#', about:'Little lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment.',tech:'React, HTML, JavaScript, SCSS',key:'2'},
+    {title:'Metaverse', githublink:'https://github.com/Lolaakinrinsola/metabnb', websiteLink:'https://metabnbzuri.vercel.app', image:'lola-portfolio/src/components/Images/metabnb.png', about:'Metaverse provide you access to luxury and affordable houses in the metaverse',tech:'React, SCSS, JavaScript, HTML',key:'3'},
+    {title:'Fylo', githublink:'https://github.com/Lolaakinrinsola/fylo-landing-page', websiteLink:'https://lolaakinrinsola.github.io/fylo-landing-page/', about:'A landing page for a company', tech:'React, SCSS, JavaScript, HTML',key:'4'},
+    {title:'Zuri FIRST', githublink:'#', websiteLink:'https://secondtaskhng.vercel.app', about:'A biodata page for zuri internship and validates form',tech:'React, SCSS, JavaScript, HTML',key:'5'},
+    {title:'My Portfolio', githublink:'https://github.com/Lolaakinrinsola/lola-portfolio', websiteLink:'https://lola-portfolio.vercel.app/',tech:'React, Chakra-UI, JavaScript, HTML',key:'6'}
   ]
   return (
     <Project id='project' >
         <Heading textAlign='center' size='2xl'>Here are some of my works</Heading>
         <Box display='flex' w='100vw' flexWrap='wrap' gap='3em' px='150px' justifyContent='center'>
           {projectsDetails.map((websites)=>(
-            <Card 
+            <Card key={websites.key}
             project={websites.title} 
             github={websites.githublink}
             website={websites.websiteLink}
